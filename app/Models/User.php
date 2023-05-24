d97e9b629051ae4076d12d4e630ae425d42c3271
s96Abrar <s96mini.cube@gmail.com>
2023-05-24 20:22:48 +0600

    Fixed user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
     
     public function profile()
     {
-        return $this->hasOne(profile::class);
+        return $this->hasOne(Profile::class);
     }
 
    
